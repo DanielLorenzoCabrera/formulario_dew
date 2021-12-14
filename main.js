23945a53010801526afceafaf7bee09795d41723
Daniel Lorenzo <juandaniellorenzocabrera@alumno.ieselrincon.es>
2021-12-14 18:19:35 +0000

    ended functionality

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", obtainInputs);
 
-const people = [];
 const options = {
     a : "Al azar",
     b : "Le indicaron la URL de la página",
-    c : "a través de un buscador",
+    c : "A través de un buscador",
     d : "Mediante un enlace desde otra página"
 }
 
 
 function obtainInputs(){
+    updateList();  
+
     const visualizarDatos = document.querySelector(".visualize");
     visualizarDatos.addEventListener("click", visualizeData);
 
@@ -18,12 +19,38 @@ function obtainInputs(){
 
     const remove = document.querySelector(".remove");
     remove.addEventListener("click", removePerson);
+
+    
+}
+
+
+function existSelection(){
+    const peopleList = document.querySelectorAll(".people");
+    let selection = false;
+    peopleList.forEach(element => {
+        if(element.selected) {
+            selection = (JSON.parse(localStorage.getItem(element.value)));
+            
+        }
+    })
+
+    return selection;
 }
 
 
 function visualizeData(){
- 
     
+    let selected = existSelection();
+   
+    selected ? 
+        alert(`
+        Nombre: ${selected.name}
+        Apellidos: ${selected.surname}
+        Email: ${selected.email}
+        Sexo: ${selected.genre}
+        Motivo: ${selected.reason}
+        Desea información sobre: ${selected.info}`)
+    : alert("nada seleccionado");
 }
 
 
@@ -39,7 +66,9 @@ function addPerson(){
 
 
 function removePerson(){
-
+    let selected = existSelection();
+    selected ? localStorage.removeItem(selected.name): alert("nada seleccionado");
+    updateList();
 }
 
 function checkEmptyFields(){
@@ -78,23 +107,22 @@ function saveData(){
     const infoOpt = document.querySelectorAll(".info");
     let info = []
     infoOpt.forEach(element => element.checked ? info.push(element.id) : '')
-    people.push({
+    window.localStorage.setItem(`${data[0].value}`, JSON.stringify({
         name : data[0].value,
         surname: data[1].value,
         email : data[2].value,
         genre: getGenre(),
         reason : options[data[3].value],
         info : info
-    })
-    console.log(people)
+    }));
+    updateList();
     
 }
 
 function existsGenre(){
     let genre = getGenre();
     let existGenre = genre !== "hombre" && genre !== "mujer" ? false : true ;
-    return existGenre;
-        
+    return existGenre;   
 }
 
 function getGenre(){
@@ -105,6 +133,21 @@ function getGenre(){
             genre = element.value;
         }
     })
-
     return genre;
+}
+
+
+function updateList(){
+    const profiles =  document.querySelector(".perfiles");
+    profiles.innerHTML = "";
+    for(let i = 0; i < localStorage.length ; i++){
+        const person = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        const opt = `<option value="${person.name}" class='people'>${person.surname}, ${person.name} | ${person.email}</option>`;
+        profiles.innerHTML = `${profiles.innerHTML} ${opt}`;
+        }
+}
+
+
+function changeSelection(){
+    console.log(this)
 }
