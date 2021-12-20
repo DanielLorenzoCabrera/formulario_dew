3d3610575c2f1be25903ab257c4279bbdf4660a9
DanielLorenzo22 <daniellorenzocabrera@gmail.com>
2021-12-20 08:22:47 +0000

    finished

--- second-form.js
+++ second-form.js
@@ -6,7 +6,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
 })
 
 
-
 function checkData(){
     const emptyFields = checkEmptyFields();
     const errors = document.querySelector(".errors");
@@ -18,7 +17,11 @@ function checkData(){
     }
 
     const wrongFields = checkWrongFields();
-
+    if(wrongFields.length > 0){
+        wrongFields.forEach(field => errors.innerHTML += field);
+        return;
+    }
+    errors.innerHTML = "<p>¡Genial! Todos los campos son <span>correctos</span></p>";
 }
 
 
@@ -28,7 +31,6 @@ function checkEmptyFields(){
     inputs.forEach(input => {
         input.value === "" ? emptyFields.push(`<p>El campo <span>${input.dataset.name}</span> está vacío.</p>`) : '';
     })
-
     return emptyFields;
 }
 
@@ -62,33 +64,36 @@ function checkWrongFields(){
                 break;
             case "importe":
                 checkImport(data) ? '' : wrongFields.push(errorMessage(input.dataset.name));
+                break;
+            case "N-tarjeta":
+                checkCardNumber(data) ? '' : wrongFields.push(errorMessage(input.dataset.name));
+                break;
+            case "month-exp":
+                checkRange(data,1,12) ? '' : wrongFields.push(errorMessage(input.dataset.name));
+                break;
+            case "year-exp":
+                checkRange(data,2001,2100) ? '' : wrongFields.push(errorMessage(input.dataset.name));
+                break;
         }
-
-
     })
-    console.log(wrongFields)
+    return wrongFields;
 }
 
-
 const validateString = (data, minLength, maxLength) => (data.length < minLength || data.length  > maxLength || containNumbers(data)) ? false : true;
-
 
 function containNumbers(string){
     for(let i = 0; i < string.length ; i++){
         if(!isNaN(parseInt(string[i]))) return true;
     }
     return false;
-
 }
 
-const errorMessage = field => `El campo ${field} es erróneo.`;
-
+const errorMessage = field => `<p>El campo <span>${field}</span> es erróneo.</p>`;
 
 function checkAge(age){
     for(let i = 0 ; i < age.length ; i++){
         if(isNaN(parseInt(age[i]))) return false;
     }
-
     if(parseInt(age) < 18 || parseInt(age) > 120) return false;
     return true;
 }
@@ -98,9 +103,7 @@ function checkRegister(data){
     return data.search(regExp) === 0 ? true : false;
 }
 
-
 const checkRange = (data, min, max) => data < min || data > max || containNaN(data) ? false : true;
-
 
 function containNaN(data){
     for(let i = 0; i < data.length ; i++){
@@ -118,10 +121,62 @@ function checkDate(date){
         year < 0 || month < 0 || month > 12 || day < 0 || day > 31  // check range
         || containNaN(year) || containNaN(month) || containNaN(day) // check nan elements
         ? false : true);
-    
 }
-
 
 const isEmpty = element => element === "" ? true : false;
 
 const checkImport = cost => ( isNaN(cost) || cost < 0 ) ? false : true ;
+
+function checkCardNumber(data){
+    if(containNaN(data)) return false;
+    const cardType = getCardType(data);
+    if(!cardType) return false;
+
+    const dataArray = data.split("");
+    const numbers = dataArray.map(number => parseInt(number));
+    dataArray.reverse();
+
+    for(let i = 0 ; i < numbers.length ; i++){
+        if((i + 1) % 2 === 0){
+            numbers[i] *= 2;
+            if(numbers[i] > 9){
+                const simplified = simplifyNumber(numbers[i]);
+                numbers[i] = simplified;
+            }
+        }
+    }
+    let total = 0;
+    numbers.forEach(number => total += number);
+ 
+    return total % 10 === 0 ? true : false;
+}
+
+function simplifyNumber(number){
+    const numberToString =  new String(number);
+    return parseInt(numberToString[0]) + parseInt(numberToString[1]);
+}
+
+function getCardType(data){
+    if(data.length < 13 || data.length > 16) return false;
+    const oneSuffix = data.substring(0,1);
+    const twoSufix = data.substring(0,2);
+    const threeSufix = data.substring(0,3);
+    const dataLength = data.length;
+
+    switch(dataLength){
+        case 13:
+            if(oneSuffix === "4") return "VS";
+            return false;
+        case 14:
+            if(twoSufix === "36" ||twoSufix === "38" || threeSufix >= "300" &&  threeSufix <= "305") return "DC";
+            return false;
+        case 15:
+            if(twoSufix === "34" || twoSufix === "37") return "AE";
+            return false;
+        case 16:
+            if(oneSuffix === "4")return "VS";
+            if(twoSufix >= "51" && twoSufix <= "55") return "MC";
+            return false;
+        default : return false;
+    }
+}
